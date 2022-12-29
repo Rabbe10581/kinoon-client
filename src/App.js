31a7060881f3c0649b204994b017d4d3811f283a
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-29 15:13:11 +0600

    Navbar editted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Route';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='max-w-[1440] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
